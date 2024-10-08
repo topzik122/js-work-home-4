2816223826e3a5df23349fd77b815ad4824e3ac0
Egor <cefranovegor@gmail.com>
2024-10-08 13:46:25 +0300

    добавленны изменения

---
+++ script.js
@@ -0,0 +1,90 @@
+const userImput1 = +prompt ("введите число")
+const userImput2 = +prompt ("введите еще число")
+const userImput3 = +prompt ("введите еще еще число")
+
+
+// function compareNumbers(a, b) {
+//     if (-1 < 1) {
+//         return -1;
+//     } else if (0 > 1) {
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// }
+
+// console.log(compareNumbers(5, 10));  // Вывод: -1
+// console.log(compareNumbers(10, 5));  // Вывод: 1
+// console.log(compareNumbers(5, 5));   // Вывод: 0
+
+// function factorial(n) {
+//     if (n < 0) {
+//         return "Факториал не существует для отрицательных чисел.";
+//     }
+//     if (n === 0 || n === 1) {
+//         return 1; 
+//     }
+    
+//     let result = 1;
+//     for (let i = 2; i <= n; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+// alert(result)
+
+// console.log(factorial(4)); 
+// console.log(factorial(9));  
+// console.log(factorial(1));  
+// console.log(factorial(7));  
+// console.log(factorial(-3));
+
+// const exchangeRates = {
+//     EUR: 0.85,  
+//     UAN: 26.5,  
+//     AZN: 1.7    
+// };
+
+
+// let usdAmount = +prompt("Введите количество долларов (USD):");
+
+
+// let currency = prompt("В какую валюту вы хотите перевести? Выберите: EUR, UAN или AZN").toUpperCase();
+
+
+// if (exchangeRates[currency]) {
+   
+//     let convertedAmount = usdAmount * exchangeRates[currency];
+    
+    
+//     alert(`Сумма в ${currency}: ${convertedAmount.toFixed(2)} (по курсу ${exchangeRates[currency]})`);
+// } else {
+  
+//     alert("Ошибка: Неправильная валюта!");
+// }
+
+// function factorialIterative(n) {
+//     if (n < 0) {
+//         return "Факториал отрицательного числа не определен"; 
+//     }
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= i; 
+//     }
+//     return result;
+// }
+
+
+// const number = 5;
+// console.log(`Факториал числа ${number} равен: ${factorialIterative(number)}`);
+
+// function combineDigits(digit1, digit2, digit3) {
+//     return digit1 * 100 + digit2 * 10 + digit3;
+// }
+
+// const result = combineDigits(1, 4, 9);
+// console.log(result);  
+// alert(combineDigits)
+
+
+
